53d37eccbb52551aa472f34dde28a83a1ef86edc
R E Broadley <rebroad+github@gmail.com>
2014-06-27 08:53:51 +0700

    Antisocial (needs CaughtUp)

--- src/util.h
+++ src/util.h
@@ -34,6 +34,8 @@ extern bool fPrintToConsole;
 extern bool fPrintToDebugLog;
 extern bool fServer;
 extern std::string strMiscWarning;
+extern bool fAntisocial;
+extern int nAntisocial;
 extern bool fLogTimestamps;
 extern bool fLogIPs;
 extern volatile bool fReopenDebugLog;
