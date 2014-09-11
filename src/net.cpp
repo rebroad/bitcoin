9711994a083d20e78764edd6ba95036320667997
R E Broadley <rebroad+github@gmail.com>
2014-09-11 10:01:41 +0700

    Reset NodeId to START_NODEID when all connections lost.

--- src/net.cpp
+++ src/net.cpp
@@ -102,7 +102,8 @@ CCriticalSection cs_vOneShots;
 std::vector<std::string> vAddedNodes;
 CCriticalSection cs_vAddedNodes;
 
-NodeId nLastNodeId = 0;
+#define START_NODEID 0
+NodeId nLastNodeId = START_NODEID;
 CCriticalSection cs_nLastNodeId;
 
 static CSemaphore *semOutbound = NULL;
@@ -1147,6 +1148,10 @@ void ThreadSocketHandler()
         if(vNodes.size() != nPrevNodeCount) {
             nPrevNodeCount = vNodes.size();
             uiInterface.NotifyNumConnectionsChanged(nPrevNodeCount);
+            if (!vNodes.size()) {
+                LogPrint("net", "NO PEERS CONNECTED. Resetting NodeId\n");
+                nLastNodeId = START_NODEID;
+            }
         }
 
         //
