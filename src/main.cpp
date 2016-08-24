5127125d6302b7f3265a3549c3393caf247b859a
R E Broadley <rebroad+github@gmail.com>
2016-08-24 13:49:04 +0700

    Don't disconnect just because a node's services have changed.

--- src/main.cpp
+++ src/main.cpp
@@ -4950,11 +4950,9 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
         }
         if (pfrom->nServicesExpected & ~pfrom->nServices)
         {
-            LogPrint("net", "peer=%d does not offer the expected services (%08x offered, %08x expected); disconnecting\n", pfrom->id, pfrom->nServices, pfrom->nServicesExpected);
+            LogPrint("net", "peer=%d does not offer the expected services (%08x offered, %08x expected)\n", pfrom->id, pfrom->nServices, pfrom->nServicesExpected);
             pfrom->PushMessage(NetMsgType::REJECT, strCommand, REJECT_NONSTANDARD,
                                strprintf("Expected to offer services %08x", pfrom->nServicesExpected));
-            pfrom->fDisconnect = true;
-            return false;
         }
 
         if (pfrom->nVersion < MIN_PEER_PROTO_VERSION)
