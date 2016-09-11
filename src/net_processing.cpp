5ae48e1ba28123f4f66ef4ff272e6a8a38272616
R E Broadley <rebroad+github@gmail.com>
2016-09-11 07:20:07 +0700

    Moorlane: Ensure addresses are sent as soon as getaddr is received. (Improve bitnodes leaderboard?)

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -2721,6 +2721,7 @@ bool static ProcessMessage(CNode* pfrom, const std::string& strCommand, CDataStr
             pfrom->PushAddress(addr, insecure_rand);
             ++nCount;
         }
+        pfrom->nNextAddrSend = 0; // Ensure it's sent right away
         LogPrint("addrman", "recv getaddr. Pushing %d (of %d) addresses. peer=%d\n", nCount, nTotal, pfrom->id);
     }
 
