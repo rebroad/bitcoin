d453666d1183a2bc3e5a9ecdb6fc7c78f373b956
R E Broadley <rebroad+github@gmail.com>
2016-09-12 16:20:54 +0700

    Move debug message to before Misbehavior message

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -1867,9 +1867,9 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
                         if (stateDummy.IsInvalid(nDos) && nDos > 0)
                         {
                             // Punish peer that gave us an invalid orphan tx
+                            LogPrint("mempool", "   invalid orphan tx %s\n", orphanHash.ToString());
                             Misbehaving(fromPeer, nDos);
                             setMisbehaving.insert(fromPeer);
-                            LogPrint("mempool", "   invalid orphan tx %s\n", orphanHash.ToString());
                         }
                         // Has inputs but not accepted to mempool
                         // Probably non-standard or insufficient fee/priority
