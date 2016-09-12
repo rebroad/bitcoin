c263991f2a7d9de971b7f32f2956bd0d95b6154a
R E Broadley <rebroad+github@gmail.com>
2016-09-12 12:58:03 +0700

    Feeler connection logging (more concise)

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -1338,7 +1338,7 @@ bool static ProcessMessage(CNode* pfrom, const std::string& strCommand, CDataStr
         {
             connman.SetServices(pfrom->addr, nServices);
         }
-        if (pfrom->nServicesExpected & ~nServices)
+        if (!pfrom->fFeeler && pfrom->nServicesExpected & ~nServices)
         {
             LogPrint("net", "recv version does not offer the expected services (%08x offered, %08x expected); disconnecting peer=%d\n", nServices, pfrom->nServicesExpected, pfrom->id);
             connman.PushMessage(pfrom, CNetMsgMaker(INIT_PROTO_VERSION).Make(NetMsgType::REJECT, strCommand, REJECT_NONSTANDARD,
