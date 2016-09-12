3abfee83cffe221a0dae4d4f013ff0f5d9126c0d
R E Broadley <rebroad+github@gmail.com>
2016-09-12 15:58:24 +0700

    Shrink debug line for new best block

--- src/main.cpp
+++ src/main.cpp
@@ -2745,15 +2745,13 @@ void static UpdateTip(CBlockIndex *pindexNew, const CChainParams& chainParams) {
             }
         }
     }
-    LogPrintf("%s: new best=%s height=%d version=0x%08x log2_work=%.8g tx=%lu date='%s' progress=%f cache=%.1fMiB(%utx)", __func__,
+    LogPrintf("%s: new best=%s (%d) ver=0x%08x date=%s tx=%lu %f%%\n", __func__,
       chainActive.Tip()->GetBlockHash().ToString(), chainActive.Height(), chainActive.Tip()->nVersion,
-      log(chainActive.Tip()->nChainWork.getdouble())/log(2.0), (unsigned long)chainActive.Tip()->nChainTx,
       DateTimeStrFormat("%Y-%m-%d %H:%M:%S", chainActive.Tip()->GetBlockTime()),
-      Checkpoints::GuessVerificationProgress(chainParams.Checkpoints(), chainActive.Tip()), pcoinsTip->DynamicMemoryUsage() * (1.0 / (1<<20)), pcoinsTip->GetCacheSize());
+      (unsigned long)chainActive.Tip()->nTx,
+      Checkpoints::GuessVerificationProgress(chainParams.Checkpoints(), chainActive.Tip())*100);
     if (!warningMessages.empty())
-        LogPrintf(" warning='%s'", boost::algorithm::join(warningMessages, ", "));
-    LogPrintf("\n");
-
+        LogPrintf(" warning='%s'\n", boost::algorithm::join(warningMessages, ", "));
 }
 
 /** Disconnect chainActive's tip. You probably want to call mempool.removeForReorg and manually re-limit mempool size after this, with cs_main held. */
