f7262b5080809d6f8692ecd9c52386ea781812d3
R E Broadley <rebroad+github@gmail.com>
2016-09-12 15:58:24 +0700

    Improve debug line for new best block

--- src/validation.cpp
+++ src/validation.cpp
@@ -2081,14 +2081,11 @@ void static UpdateTip(CBlockIndex *pindexNew, const CChainParams& chainParams) {
             }
         }
     }
-    LogPrintf("%s: new best=%s height=%d version=0x%08x log2_work=%.8g tx=%lu age=%s progress=%f cache=%.1fMiB(%utx)", __func__,
+    LogPrintf("%s: new best=%s (%d) ver=0x%08x age=%s tx=%lu\n", __func__,
       chainActive.Tip()->GetBlockHash().ToString(), chainActive.Height(), chainActive.Tip()->nVersion,
-      log(chainActive.Tip()->nChainWork.getdouble())/log(2.0), (unsigned long)chainActive.Tip()->nChainTx,
-      strAge(chainActive.Tip()),
-      Checkpoints::GuessVerificationProgress(chainParams.Checkpoints(), chainActive.Tip()), pcoinsTip->DynamicMemoryUsage() * (1.0 / (1<<20)), pcoinsTip->GetCacheSize());
+      strAge(chainActive.Tip()), (unsigned long)chainActive.Tip()->nTx);
     if (!warningMessages.empty())
-        LogPrintf(" warning='%s'", boost::algorithm::join(warningMessages, ", "));
-    LogPrintf("\n");
+        LogPrintf(" warning='%s'\n", boost::algorithm::join(warningMessages, ", "));
 
 }
 
