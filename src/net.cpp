8fe7a5e4a2a0b31c11ac5191bfc84e348374d92e
R E Broadley <rebroad+github@gmail.com>
2016-09-15 14:16:44 +0700

    Log when evicting peers

--- src/net.cpp
+++ src/net.cpp
@@ -951,6 +951,7 @@ bool CConnman::AttemptToEvictConnection()
     LOCK(cs_vNodes);
     for(std::vector<CNode*>::const_iterator it(vNodes.begin()); it != vNodes.end(); ++it) {
         if ((*it)->GetId() == evicted) {
+            LogPrint("net", "Evicting peer %d\n", evicted);
             (*it)->fDisconnect = true;
             return true;
         }
