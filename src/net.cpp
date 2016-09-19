8c5201d00e2a8bcea09db347b423f9fdc99b0624
R E Broadley <rebroad+github@gmail.com>
2016-09-19 10:08:03 +0100

    Moorlane: Seed from bitnodes

--- src/net.cpp
+++ src/net.cpp
@@ -39,6 +39,7 @@
 #include <boost/thread.hpp>
 
 #include <math.h>
+#include <bitnodes.h>
 
 // Dump addresses to peers.dat and banlist.dat every 15 minutes (900s)
 #define DUMP_ADDRESSES_INTERVAL 900
@@ -1467,9 +1468,42 @@ void MapPort(bool)
 }
 #endif
 
+void CConnman::ThreadBitnodesAddressSeed()
+{
+    // Get nodes from websites offering Bitnodes API
+    if ((addrman.size() > 0) &&
+        (!GetBoolArg("-forcebitnodes", DEFAULT_FORCEBITNODES))) {
+        MilliSleep(11 * 1000);
+        LOCK(cs_vNodes);
+        if (vNodes.size() >= 2) {
+            LogPrintf("P2P peers available. Skipped Bitnodes seeding.\n");
+            return;
+        }
+    }
 
+    LogPrintf("Loading addresses from Bitnodes API\n");
 
+    std::vector<std::string> vIPs;
+    std::vector<CAddress> vAdd;
+    if (GetLeaderboardFromBitnodes(vIPs)) {
+        int port;
+        BOOST_FOREACH(const std::string &seed, vIPs) {
+            std::string host;
+            SplitHostPort(seed, port, host);
+	    CNetAddr resolved;
+	    if (LookupHost(host.c_str(), resolved, false)) {
+                CAddress addr = CAddress(CService(resolved, port), nRelevantServices);
+                addr.nTime = GetTime();
+                vAdd.push_back(addr);
+            }
+        }
+        CNetAddr resolved;
+        LookupHost("bitnodes.21.co", resolved, false);
+        addrman.Add(vAdd, resolved, 0);
+    }
 
+    LogPrintf("%d addresses found from Bitnodes API\n", vAdd.size());
+}
 
 
 static std::string GetDNSHost(const CDNSSeedData& data, ServiceFlags* requiredServiceBits)
@@ -2165,6 +2199,11 @@ bool CConnman::Start(boost::thread_group& threadGroup, CScheduler& scheduler, st
     //
     // Start threads
     //
+
+    if (!GetBoolArg("-bitnodes", true))
+        LogPrintf("Bitnodes API seeding disabled\n");
+    else
+        threadGroup.create_thread(boost::bind(&TraceThread<boost::function<void()> >, "bitnodes", boost::function<void()>(boost::bind(&CConnman::ThreadBitnodesAddressSeed, this))));
 
     if (!GetBoolArg("-dnsseed", true))
         LogPrintf("DNS seeding disabled\n");
--- src/net.h
+++ src/net.h
@@ -76,6 +76,8 @@ static const uint64_t MAX_UPLOAD_TIMEFRAME = 60 * 60 * 24;
 /** Default for blocks only*/
 static const bool DEFAULT_BLOCKSONLY = false;
 
+static const bool DEFAULT_FORCEBITNODES = false;
+
 static const bool DEFAULT_FORCEDNSSEED = false;
 static const size_t DEFAULT_MAXRECEIVEBUFFER = 5 * 1000;
 static const size_t DEFAULT_MAXSENDBUFFER    = 1 * 1000;
@@ -333,6 +335,7 @@ private:
     void AcceptConnection(const ListenSocket& hListenSocket);
     void ThreadSocketHandler();
     void ThreadDNSAddressSeed();
+    void ThreadBitnodesAddressSeed();
 
     uint64_t CalculateKeyedNetGroup(const CAddress& ad);
 
