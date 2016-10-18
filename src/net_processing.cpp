35652ea05c8aeba30cd1a752130568f78ee87e63
R E Broadley <rebroad+github@gmail.com>
2016-10-18 10:23:51 +0700

    Headers announcement for nodes that can do headers.

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -181,6 +181,8 @@ struct CNodeState {
     bool fPreferredDownload;
     //! Whether this peer wants invs or headers (when possible) for block announcements.
     bool fPreferHeaders;
+    //! Whether this peer can process headers for block announcements.
+    bool fCanDoHeaders;
     //! Whether this peer wants invs or cmpctblocks (when possible) for block announcements.
     bool fPreferHeaderAndIDs;
     /**
@@ -216,6 +218,7 @@ struct CNodeState {
         nBlocksInFlightValidHeaders = 0;
         fPreferredDownload = false;
         fPreferHeaders = false;
+        fCanDoHeaders = false;
         fPreferHeaderAndIDs = false;
         fProvidesHeaderAndIDs = false;
         fHaveWitness = false;
@@ -1429,6 +1432,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
     {
         LOCK(cs_main);
         State(pfrom->GetId())->fPreferHeaders = true;
+        State(pfrom->GetId())->fCanDoHeaders = true;
     }
 
     else if (strCommand == NetMsgType::SENDCMPCT)
@@ -1641,6 +1645,8 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
 
     else if (strCommand == NetMsgType::GETHEADERS)
     {
+        State(pfrom->GetId())->fCanDoHeaders = true;
+
         CBlockLocator locator;
         uint256 hashStop;
         vRecv >> locator >> hashStop;
@@ -2922,7 +2928,7 @@ bool SendMessages(CNode* pto, CConnman& connman)
             // add all to the inv queue.
             LOCK(pto->cs_inventory);
             vector<CBlock> vHeaders;
-            bool fRevertToInv = ((!state.fPreferHeaders &&
+            bool fRevertToInv = ((!state.fCanDoHeaders &&
                                  (!state.fPreferHeaderAndIDs || pto->vBlockHashesToAnnounce.size() > 1)) ||
                                 pto->vBlockHashesToAnnounce.size() > MAX_BLOCKS_TO_ANNOUNCE);
             CBlockIndex *pBestIndex = NULL; // last header queued for delivery
@@ -2995,7 +3001,7 @@ bool SendMessages(CNode* pto, CConnman& connman)
                     LogPrint("block", "send cmpctblock %s size=%d peer=%d\n",
                             vHeaders.front().GetHash().ToString(), nSize, pto->id);
                     state.pindexBestHeaderSent = pBestIndex;
-                } else if (state.fPreferHeaders) {
+                } else if (state.fCanDoHeaders) {
                     int nSize = connman.PushMessage(pto, msgMaker.Make(NetMsgType::HEADERS, vHeaders));
                     if (vHeaders.size() > 1) {
                         LogPrint("block", "send %u headers (%d to %d) size=%d peer=%d\n",
