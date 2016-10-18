82b2c2177e37eb0b78c46e22ab808bab81edaae2
R E Broadley <rebroad+github@gmail.com>
2016-10-18 10:24:40 +0700

    Bypass PushInventory to announce blocks faster

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -3231,7 +3231,9 @@ bool SendMessages(CNode* pto, CConnman& connman, const std::atomic<bool>& interr
 
                     // If the peer's chain has this block, don't inv it back.
                     if (!PeerHasHeader(&state, pindex)) {
-                        pto->PushInventory(CInv(MSG_BLOCK, hashToAnnounce));
+                        std::vector<CInv> vInv;
+                        vInv.push_back(CInv(MSG_BLOCK, hashToAnnounce));
+                        connman.PushMessage(pto, msgMaker.Make(NetMsgType::INV, vInv));
                         LogPrint("block", "send inv block %s %s peer=%d\n",
                             hashToAnnounce.ToString(), strBlkInfo(pindex), pto->id);
                     }
