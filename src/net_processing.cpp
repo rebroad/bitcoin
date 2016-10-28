896eb8e9bff1eaf27ec0af347b7b3c940e3181af
R E Broadley <rebroad+github@gmail.com>
2016-10-28 22:57:32 +0700

    Show blocks invs sent to nodes seemingly uninterested in blocks

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -181,6 +181,8 @@ struct CNodeState {
     bool fPreferHeaders;
     //! Whether this peer can process headers for block announcements.
     bool fCanDoHeaders;
+    //! Whether this peer is interested in block invs
+    bool fSentGetblocks;
     //! Whether this peer wants invs or cmpctblocks (when possible) for block announcements.
     bool fPreferHeaderAndIDs;
     /**
@@ -1728,6 +1730,7 @@ bool static ProcessMessage(CNode* pfrom, const std::string& strCommand, CDataStr
 
     else if (strCommand == NetMsgType::GETBLOCKS)
     {
+        State(pfrom->id)->fSentGetblocks = true;
         CBlockLocator locator;
         uint256 hashStop;
         vRecv >> locator >> hashStop;
@@ -3259,7 +3262,7 @@ bool SendMessages(CNode* pto, CConnman& connman, const std::atomic<bool>& interr
                         std::vector<CInv> vInv;
                         vInv.push_back(CInv(MSG_BLOCK, hashToAnnounce));
                         connman.PushMessage(pto, msgMaker.Make(NetMsgType::INV, vInv));
-                        LogPrint("block", "send inv block %s (%d) peer=%d\n",
+                        LogPrint("block", "send inv%s block %s (%d) peer=%d\n", state.fSentGetblocks ? "" : "*",
                             hashToAnnounce.ToString(), pindex->nHeight, pto->id);
                     }
                 }
