62ab4bddf0a3be5e396fbf743e7a0e3ea02413d3
R E Broadley <rebroad+github@gmail.com>
2016-10-28 12:59:21 +0700

    remove blank lines

--- src/net.cpp
+++ src/net.cpp
@@ -1545,17 +1545,6 @@ void CConnman::ThreadDNSAddressSeed()
     LogPrintf("%d addresses found from DNS seeds\n", found);
 }
 
-
-
-
-
-
-
-
-
-
-
-
 void CConnman::DumpAddresses()
 {
     int64_t nStart = GetTimeMillis();
