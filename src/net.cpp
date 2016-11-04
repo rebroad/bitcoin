ae9dc63498c77717dd00ab0316bea0221d32b005
R E Broadley <rebroad+github@gmail.com>
2016-11-04 08:29:41 +0800

    Make peer selection require more Witness nodes once SegWit activates

--- src/net.cpp
+++ src/net.cpp
@@ -1835,7 +1835,7 @@ void CConnman::ThreadOpenConnections()
             // and at least 6 once SegWit has activated
             ServiceFlags nRequiredServices = nRelevantServices;
             if ((addr.nServices & nRelevantServices) != nRequiredServices && (nTries < 40 &&
-                    nRelevant < 5) && !fFeeler)
+                    (nRelevant < 5 || (fWitnessActive && nRelevant < 7))) && !fFeeler)
                 continue;
 
             // do not allow non-default ports, unless after 50 invalid addresses selected already
