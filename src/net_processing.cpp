d392f048779c461e4382c8bfd19f39a74c1a34e3
R E Broadley <rebroad+github@gmail.com>
2016-11-04 23:23:59 +0800

    No "Unknown message" for alerts.

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -2648,6 +2648,12 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
     }
 
 
+    else if (strCommand == NetMsgType::ALERT)
+    {
+	;;
+    }
+
+
     else if (strCommand == NetMsgType::FILTERLOAD)
     {
         CBloomFilter filter;
