18af247eef22eab24880f388548484dedeee3913
R E Broadley <rebroad+github@gmail.com>
2016-11-24 13:36:58 +0700

    No need to check fDebug for simple LogPrint

--- src/net.cpp
+++ src/net.cpp
@@ -2194,9 +2194,7 @@ void Discover(boost::thread_group& threadGroup)
 
 void CConnman::SetNetworkActive(bool active)
 {
-    if (fDebug) {
-        LogPrint("net", "SetNetworkActive: %s\n", active);
-    }
+    LogPrint("net", "SetNetworkActive: %s\n", active);
 
     if (!active) {
         fNetworkActive = false;
