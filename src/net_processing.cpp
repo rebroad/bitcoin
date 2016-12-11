f487c7a60d7ecb1f6916cc02879c3681a3eab7e2
R E Broadley <rebroad+github@gmail.com>
2016-12-11 08:34:31 +0700

    Allow first cmpctblock received to be processed (rather than first requested).
    Speeds up block propagation as avoids an unncessary wait for the first
    requested cmpctblock and instead allows cmpctblocks sent as announcement
    to be processed when they arrive before the requested cmpctblock.
    
    Fixes 9242

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -2028,8 +2028,7 @@ bool static ProcessMessage(CNode* pfrom, std::string strCommand, CDataStream& vR
         // We want to be a bit conservative just to be extra careful about DoS
         // possibilities in compact block processing...
         if (pindex->nHeight <= chainActive.Height() + 2) {
-            if ((!fAlreadyInFlight && nodestate->nBlocksInFlight < MAX_BLOCKS_IN_TRANSIT_PER_PEER) ||
-                 (fAlreadyInFlight && blockInFlightIt->second.first == pfrom->GetId())) {
+            if ((!fAlreadyInFlight && nodestate->nBlocksInFlight < MAX_BLOCKS_IN_TRANSIT_PER_PEER) || fAlreadyInFlight) {
                 std::list<QueuedBlock>::iterator* queuedBlockIt = NULL;
                 if (!MarkBlockAsInFlight(pfrom->GetId(), pindex->GetBlockHash(), chainparams.GetConsensus(), pindex, &queuedBlockIt)) {
                     if (!(*queuedBlockIt)->partialBlock)
