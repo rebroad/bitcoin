b8b110871181aa69a50bf00c079b55d90208d178
R E Broadley <rebroad+github@gmail.com>
2016-12-20 08:55:16 +0700

    fixup conformity

--- src/net.cpp
+++ src/net.cpp
@@ -674,7 +674,7 @@ bool CNode::ReceiveMsgBytes(const char *pch, unsigned int nBytes, bool& complete
                 return false;
 
         if (msg.in_data && msg.hdr.nMessageSize > MAX_PROTOCOL_MESSAGE_LENGTH) {
-            LogPrint("net", "Oversized message from peer=%i, disconnecting\n", GetId());
+            LogPrint("net", "Oversized message. Disconnecting peer=%i\n", GetId());
             return false;
         }
 
