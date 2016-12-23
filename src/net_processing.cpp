070984bbe7e2af1703878eb46c2ab1b96b7e43ad
R E Broadley <rebroad+github@gmail.com>
2016-12-23 13:19:43 +0700

    Debug update of LastCommonBlock

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -3476,7 +3476,10 @@ bool SendMessages(CNode* pto, CConnman& connman, std::atomic<bool>& interruptMsg
                 state.nBlockPaused = -9;
             }
             NodeId staller = -1;
+            const CBlockIndex *prevCommonBlock = state.pindexLastCommonBlock;
             FindNextBlocksToDownload(pto->GetId(), MAX_BLOCKS_IN_TRANSIT_PER_PEER - state.nBlocksInFlight, vToDownload, staller, consensusParams);
+            if (prevCommonBlock != state.pindexLastCommonBlock)
+                LogPrint("blocklastcommon", "%s: LastCommonBlock (%s) -> (%s) peer=%d\n", __func__, strHeight(prevCommonBlock), strHeight(state.pindexLastCommonBlock), pto->id);
             BOOST_FOREACH(const CBlockIndex *pindex, vToDownload) {
                 uint32_t nFetchFlags = GetFetchFlags(pto, pindex->pprev, consensusParams);
                 vGetData.push_back(CInv(MSG_BLOCK | nFetchFlags, pindex->GetBlockHash()));
