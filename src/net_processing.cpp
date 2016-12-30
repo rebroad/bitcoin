807fdfcbc66b088301e5af208bbeece8c615661d
R E Broadley <rebroad+github@gmail.com>
2016-12-30 22:18:58 +0700

    better pong debug message for short payload

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -2763,7 +2763,8 @@ bool static ProcessMessage(CNode* pfrom, std::string strCommand, CDataStream& vR
         } else {
             // This is most likely a bug in another implementation somewhere; cancel this ping
             bPingFinished = true;
-            sProblem = "Short payload";
+            LogPrint("net", "recv pong: Short payload size=%d expected=%d ping?=%dms peer=%d\n", nAvail, sizeof(nonce),
+                   (pingUsecEnd - pfrom->nPingUsecStart) / 1000, pfrom->id);
         }
 
         if (!(sProblem.empty())) {
