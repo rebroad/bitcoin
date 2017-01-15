28c654a99494d41abe7a9fdf306ad2d7a1725c52
R E Broadley <rebroad+github@gmail.com>
2017-01-16 01:53:32 +0800

    Startup debug

--- src/init.cpp
+++ src/init.cpp
@@ -703,7 +703,9 @@ void ThreadImport(std::vector<boost::filesystem::path> vImportFiles)
         StartShutdown();
     }
     } // End scope of CImportingNow
+    LogPrintf("%s: Starting LoadMempool()\n", __func__);
     LoadMempool();
+    LogPrintf("%s: Finished LoadMempool()\n", __func__);
     fDumpMempoolLater = !fRequestShutdown;
 }
 
