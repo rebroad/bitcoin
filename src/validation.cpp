397f8ee80ca69a9f468dd53d88cf42ab71035b4c
R E Broadley <rebroad+github@gmail.com>
2017-02-02 08:09:21 +0800

    WIP allow shutdown during startup

--- src/validation.cpp
+++ src/validation.cpp
@@ -3577,6 +3577,8 @@ bool static LoadBlockIndexDB(const CChainParams& chainparams)
     LogPrintf("%s: building vSortedByHeight\n", __func__);
     BOOST_FOREACH(const PAIRTYPE(uint256, CBlockIndex*)& item, mapBlockIndex)
     {
+        if (ShutdownRequested())
+            return false;
         CBlockIndex* pindex = item.second;
         vSortedByHeight.push_back(std::make_pair(pindex->nHeight, pindex));
     }
@@ -3585,6 +3587,8 @@ bool static LoadBlockIndexDB(const CChainParams& chainparams)
     LogPrintf("%s: parsing vSortedByHeight\n", __func__);
     BOOST_FOREACH(const PAIRTYPE(int, CBlockIndex*)& item, vSortedByHeight)
     {
+        if (ShutdownRequested())
+            return false;
         CBlockIndex* pindex = item.second;
         pindex->nChainWork = (pindex->pprev ? pindex->pprev->nChainWork : 0) + GetBlockProof(*pindex);
         pindex->nTimeMax = (pindex->pprev ? std::max(pindex->pprev->nTimeMax, pindex->nTime) : pindex->nTime);
