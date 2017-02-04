ed343a35008103de76826e0aaebacb7ca1944941
R E Broadley <rebroad+github@gmail.com>
2017-02-04 12:03:17 +0800

    TEMP debug interruptMsgProc

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -3081,8 +3081,10 @@ bool ProcessMessages(CNode* pfrom, CConnman& connman, const std::atomic<bool>& i
         try
         {
             fRet = ProcessMessage(pfrom, strCommand, vRecv, msg.nTime, chainparams, connman, interruptMsgProc);
-            if (interruptMsgProc)
+            if (interruptMsgProc) {
+                LogPrintf("%s: interruptMsgProc\n", __func__);
                 return false;
+            }
             if (!pfrom->vRecvGetData.empty())
                 fMoreWork = true;
         }
