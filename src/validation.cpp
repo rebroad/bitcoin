73f82824504477f8dd5b521f64f8de575fa2a5ba
R E Broadley <rebroad@gmail.com>
2021-02-26 14:50:07 +0000

    fixup TEMP try fixing corrupt chain

--- src/validation.cpp
+++ src/validation.cpp
@@ -659,7 +659,7 @@ bool MemPoolAccept::PreChecks(ATMPArgs& args, Workspace& ws)
                 }
             }
             // Otherwise assume this might be an orphan tx for which we just haven't seen parents yet
-            return state.Invalid(TxValidationResult::TX_MISSING_INPUTS, "bad-txns-inputs-missingorspent");
+            return state.Invalid(TxValidationResult::TX_MISSING_INPUTS, "bad-txns-inputs-missingorspent"); // REBTODO
         }
     }
 
