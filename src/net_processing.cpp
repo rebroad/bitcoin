5c21488772504f68af9413b3d928e425a35ad7f0
R E Broadley <rebroad@gmail.com>
2021-02-26 14:50:51 +0000

    TEMP check if txs are being sent during IBD

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -5017,6 +5017,7 @@ bool PeerManagerImpl::SendMessages(CNode* pto)
                         // Responses to MEMPOOL requests bypass the m_recently_announced_invs filter.
                         vInv.push_back(inv);
                         if (vInv.size() == MAX_INV_SZ) {
+                            LogPrintf("send %d mempool tx invs peer=%d\n", MAX_INV_SZ, pto->GetId());
                             m_connman.PushMessage(pto, msgMaker.Make(NetMsgType::INV, vInv));
                             vInv.clear();
                         }
@@ -5089,6 +5090,8 @@ bool PeerManagerImpl::SendMessages(CNode* pto)
                             }
                         }
                         if (vInv.size() == MAX_INV_SZ) {
+                            if (m_chainman.ActiveChainstate().IsInitialBlockDownload())
+                                LogPrintf("Send %d tx invs to peer=%d\n", MAX_INV_SZ, pto->GetId());
                             m_connman.PushMessage(pto, msgMaker.Make(NetMsgType::INV, vInv));
                             vInv.clear();
                         }
@@ -5104,8 +5107,11 @@ bool PeerManagerImpl::SendMessages(CNode* pto)
                     }
                 }
         }
-        if (!vInv.empty())
+        if (!vInv.empty()) {
+            if (m_chainman.ActiveChainstate().IsInitialBlockDownload())
+                LogPrintf("Send %d tx invs to peer=%d\n", vInv.size(), pto->GetId());
             m_connman.PushMessage(pto, msgMaker.Make(NetMsgType::INV, vInv));
+        }
 
         // Detect whether we're stalling
         if (state.m_stalling_since.count() && state.m_stalling_since < current_time - BLOCK_STALLING_TIMEOUT) {
