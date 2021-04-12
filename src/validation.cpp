591c837aa1e090c5b65b0a272486467b3eb99133
R E Broadley <rebroad+github@gmail.com>
2021-04-13 00:35:38 +0100

    WIP - display highest Sat/B TX after UpdateTip

--- src/validation.cpp
+++ src/validation.cpp
@@ -3855,7 +3855,7 @@ bool ChainstateManager::ProcessNewBlock(const CChainParams& chainparams, const s
 {
     AssertLockNotHeld(cs_main);
 
-    {
+    { // TODO: Calculate the lowest Sat/B TX still in the mempool after the TXs in this block have removed the TXs
         CBlockIndex *pindex = nullptr;
         if (fNewBlock) *fNewBlock = false;
         BlockValidationState state;
