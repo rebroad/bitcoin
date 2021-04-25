bd972da4d477496e3947b929e5f1de41dac1a422
R E Broadley <rebroad+github@gmail.com>
2021-04-25 21:44:32 +0100

    Conform to ISO standard

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -831,14 +831,14 @@ QString formatNiceTimeOffset(qint64 secs)
 
 QString formatBytes(uint64_t bytes)
 {
-    if(bytes < 1024)
+    if(bytes < 1000)
         return QString(QObject::tr("%1 B")).arg(bytes);
-    if(bytes < 1024 * 1024)
-        return QString(QObject::tr("%1 KB")).arg(bytes / 1024);
-    if(bytes < 1024 * 1024 * 1024)
-        return QString(QObject::tr("%1 MB")).arg(bytes / 1024 / 1024);
+    if(bytes < 1000 * 1000)
+        return QString(QObject::tr("%1 KB")).arg(bytes / 1000);
+    if(bytes < 1000 * 1000 * 1000)
+        return QString(QObject::tr("%1 MB")).arg(bytes / 1000 / 1000);
 
-    return QString(QObject::tr("%1 GB")).arg(bytes / 1024 / 1024 / 1024);
+    return QString(QObject::tr("%1 GB")).arg(bytes / 1000 / 1000 / 1000);
 }
 
 qreal calculateIdealFontSize(int width, const QString& text, QFont font, qreal minPointSize, qreal font_size) {
