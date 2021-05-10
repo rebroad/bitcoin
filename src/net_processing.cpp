c4d11bf8ff28b597cf83df7d36d2f655688847f7
R E Broadley <rebroad+github@gmail.com>
2021-05-10 12:23:38 +0100

    Don't SendMessages() if shutting down

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -23,6 +23,7 @@
 #include <random.h>
 #include <reverse_iterator.h>
 #include <scheduler.h>
+#include <shutdown.h>
 #include <streams.h>
 #include <tinyformat.h>
 #include <txmempool.h>
@@ -4049,7 +4050,7 @@ bool PeerManager::SendMessages(CNode* pto)
     if (MaybeDiscourageAndDisconnect(*pto)) return true;
 
     // Don't send anything until the version handshake is complete
-    if (!pto->fSuccessfullyConnected || pto->fDisconnect)
+    if (!pto->fSuccessfullyConnected || pto->fDisconnect || ShutdownRequested())
         return true;
 
     // If we get here, the outgoing message serialization version is set and can't change.
