cf3f2556b573a04071e955920bf584c7decd089f
R E Broadley <github.com@esuza.com>
2021-07-29 22:22:12 +0200

    Shorten connection type in debug window

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -692,16 +692,16 @@ QString ConnectionTypeToQString(ConnectionType conn_type, bool prepend_direction
     switch (conn_type) {
     case ConnectionType::INBOUND: return prefix;
     //: Peer connection type that relays all network information.
-    case ConnectionType::OUTBOUND_FULL_RELAY: return prefix + QObject::tr("Full Relay");
+    case ConnectionType::OUTBOUND_FULL_RELAY: return prefix + QObject::tr("Full");
     /*: Peer connection type that relays network information about
         blocks and not transactions or addresses. */
-    case ConnectionType::BLOCK_RELAY: return prefix + QObject::tr("Block Relay");
+    case ConnectionType::BLOCK_RELAY: return prefix + QObject::tr("Block");
     //: Peer connection type established manually through one of several methods.
     case ConnectionType::MANUAL: return prefix + QObject::tr("Manual");
     //: Short-lived peer connection type that tests the aliveness of known addresses.
     case ConnectionType::FEELER: return prefix + QObject::tr("Feeler");
     //: Short-lived peer connection type that solicits known addresses from a peer.
-    case ConnectionType::ADDR_FETCH: return prefix + QObject::tr("Address Fetch");
+    case ConnectionType::ADDR_FETCH: return prefix + QObject::tr("Addr");
     } // no default case, so the compiler can warn about missing cases
     assert(false);
 }
