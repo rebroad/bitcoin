2f9ae75c22a167cb06dcfc8d14222ca820095688
R E Broadley <github.com@esuza.com>
2021-09-16 20:16:46 +0200

    better

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -63,15 +63,12 @@ size_t CTxMemPoolEntry::GetTxSize() const
 
 unsigned int CTxMemPoolEntry::GetHeight(const CChain& active_chain) const
 {
-    //int64_t now = GetTimeSeconds();
     CBlockIndex* ret = active_chain.FindEarliestAtLeast(nTime, 0);
     unsigned int newEntryHeight = ret ? ret->nHeight-1 : active_chain.Height();
-    std::string strSame;
-    if ((entryHeight != newEntryHeight) && (nodeid != -2)) // Hide LoadMempool entries as we know they differ
-        LogPrintf("%s: nTime=%s entryHeight=%d newEntryHeight=%d ret=%d peer=%d\n", __func__, FormatISO8601DateTime(nTime), entryHeight, newEntryHeight, ret ? 1 : 0, nodeid);
+    //if ((entryHeight != newEntryHeight) && (nodeid != -2)) // Hide LoadMempool entries as we know they differ
+    //    LogPrintf("%s: nTime=%s entryHeight=%d newEntryHeight=%d ret=%d peer=%d\n", __func__, FormatISO8601DateTime(nTime), entryHeight, newEntryHeight, ret ? 1 : 0, nodeid);
 
-    //return ret ? ret->nHeight : active_chain.Height();
-    return entryHeight;
+    return newEntryHeight;
 }
 
 // Update the given tx for any in-mempool descendants.
