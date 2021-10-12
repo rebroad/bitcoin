19bcdc237431838874e86c5f647e9b481687d946
R E Broadley <github.com@esuza.com>
2021-10-12 19:22:57 +0200

    Don't LoadMempool if shutting down

--- src/node/blockstorage.cpp
+++ src/node/blockstorage.cpp
@@ -560,7 +560,8 @@ void ThreadImport(ChainstateManager& chainman, std::vector<fs::path> vImportFile
             return;
         }
     } // End scope of CImportingNow
-    chainman.ActiveChainstate().LoadMempool(args);
+    if (!ShutdownRequested())
+        chainman.ActiveChainstate().LoadMempool(args);
     while(!ShutdownRequested()) {
         std::this_thread::sleep_for(std::chrono::seconds(1));
         chainman.ActiveChainstate().LoadMempoolCache(args);
