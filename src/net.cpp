ef49e780970167d0c89ba892aa318015dfc3e4fb
R E Broadley <github.com@esuza.com>
2021-10-12 20:04:01 +0200

    TEMP debug anchors loading

--- src/net.cpp
+++ src/net.cpp
@@ -2266,9 +2266,13 @@ void CConnman::ThreadOpenConnections(const std::vector<std::string> connect)
         {
             static int nLastOutboundCount = MAX_OUTBOUND_FULL_RELAY_CONNECTIONS; // On startup read anchors
             int nOutboundCount = nOutboundFullRelay + nOutboundBlockRelay;
-            if (nOutboundCount > nLastOutboundCount)
+            if (nOutboundCount > nLastOutboundCount) {
+                int nLastLast = nLastOutboundCount;
                 nLastOutboundCount = std::min(nOutboundCount, (int)MAX_OUTBOUND_FULL_RELAY_CONNECTIONS +
                     (int)MAX_BLOCK_RELAY_ONLY_ANCHORS);
+                if (nLastLast != nLastOutboundCount)
+                    LogPrintf("anchor LOC %d -> %d\n", nLastLast, nLastOutboundCount);
+            }
             if (!m_anchors.empty() && (anchor == 0 || nOutboundCount)) {
                 anchor++;
                 const CAddress addr = m_anchors.back();
@@ -2309,6 +2313,10 @@ void CConnman::ThreadOpenConnections(const std::vector<std::string> connect)
             if (m_anchors.empty() && ((nAnchorTryAgain == 1 && nOutboundCount > 0) ||
                     (nAnchorTryAgain > 1 && nPeersIBD <= 1 && nOutboundCount >= 2) ||
                     (nOutboundCount < (nLastOutboundCount+1)*2/3))) { // or a sudden drop in connections
+                if (nOutboundCount < (nLastOutboundCount+1)*2/3)
+                    LogPrintf("Outbound count dropped (%d < %d) LOC=%d\n", nOutboundCount, (nLastOutboundCount+1)*2/3, nLastOutboundCount);
+                else
+                    LogPrintf("ATA=%D OC=%d PIBD=%d\n", nAnchorTryAgain, nOutboundCount, nPeersIBD);
                 nLastOutboundCount = nOutboundCount;
                 if (nAnchorTryAgain >= 0 && !interruptNet.sleep_for(std::chrono::milliseconds(500)))
                         return;
