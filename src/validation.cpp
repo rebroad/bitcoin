2333f6d68f9a74704abcfac29b7d5464a806e4df
R E Broadley <github.com@esuza.com>
2021-10-20 20:44:10 +0200

    TEMP debug LoadMempool()

--- src/validation.cpp
+++ src/validation.cpp
@@ -3878,7 +3878,11 @@ bool BlockManager::LoadBlockIndexDB(std::set<CBlockIndex*, CBlockIndexWorkCompar
 
 void CChainState::LoadMempool(const ArgsManager& args)
 {
-    if (!m_mempool) return;
+    if (!m_mempool) {
+        LogPrintf("%s: no mempool. Exiting\n", __func__);
+        return;
+    }
+    LogPrintf("%s: Start\n", __func__);
     if (args.GetBoolArg("-persistmempool", DEFAULT_PERSIST_MEMPOOL)) {
         ::LoadMempool(*m_mempool, "mempool.dat", *this);
         ::LoadMempool(*m_mempool, "mempool.dat.2", *this);
