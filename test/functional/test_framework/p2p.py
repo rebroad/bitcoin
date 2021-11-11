372927e782ac1d223d099ed525cc97c0f163c1df
Gleb Naumenko <naumenko.gs@gmail.com>
2021-11-11 09:14:09 +0300

    Add functional test support for sendrecon message
    This is needed to pass existing functional tests.

--- test/functional/test_framework/p2p.py
+++ test/functional/test_framework/p2p.py
@@ -59,6 +59,7 @@ from test_framework.messages import (
     msg_sendaddrv2,
     msg_sendcmpct,
     msg_sendheaders,
+    msg_sendrecon,
     msg_tx,
     MSG_TX,
     MSG_TYPE_MASK,
@@ -120,6 +121,7 @@ MESSAGEMAP = {
     b"sendaddrv2": msg_sendaddrv2,
     b"sendcmpct": msg_sendcmpct,
     b"sendheaders": msg_sendheaders,
+    b"sendrecon": msg_sendrecon,
     b"tx": msg_tx,
     b"verack": msg_verack,
     b"version": msg_version,
@@ -415,6 +417,7 @@ class P2PInterface(P2PConnection):
     def on_sendaddrv2(self, message): pass
     def on_sendcmpct(self, message): pass
     def on_sendheaders(self, message): pass
+    def on_sendrecon(self, message): pass
     def on_tx(self, message): pass
     def on_wtxidrelay(self, message): pass
 
