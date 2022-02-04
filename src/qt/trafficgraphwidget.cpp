b0a254e557ac7e4b51d8536dd67506f486cd478c
R E Broadley <github.com@esuza.com>
2022-02-04 12:10:44 +0000

    fix

--- src/qt/trafficgraphwidget.cpp
+++ src/qt/trafficgraphwidget.cpp
@@ -257,11 +257,11 @@ void TrafficGraphWidget::updateDisplay()
         if (ttpoint >= 0) { // Remove the yellow circle if the ToolTip has gone due to mouse moving elsewhere.
             if (last_fToggle == fToggle) { // Not lost due to a toggle
                 ttpoint = -1;
+                LogPrintf("%s: InVisible. Setting ttpoint = -1. age=%d Call update()\n", __func__, GetTime() - tt_time);
             } else {
                 last_fToggle = fToggle;
-                LogPrintf("%s: InVisible. Setting ttpoint = -1. age=%d Call update()\n", __func__, GetTime() - tt_time);
-            } else
                 LogPrintf("%s: InVisible but toggled. Call update()\n", __func__);
+            }
             fUpdate = true;
         }
     } else if (ttpoint >= 0 && GetTime() >= tt_time + 9) { // ToolTip is about to expire so refresh it.
