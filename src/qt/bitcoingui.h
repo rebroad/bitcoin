8866fddb2938eb9718b134e03b07921aacd7b0f3
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-02-08 16:21:45 +0200

    qt, refactor: Replace BitcoinGUI::trayIconActivated with a lambda

--- src/qt/bitcoingui.h
+++ src/qt/bitcoingui.h
@@ -307,10 +307,6 @@ public Q_SLOTS:
     void showHelpMessageClicked();
     /** Show mempool stats window */
     void showMempoolStatsWindow();
-#ifndef Q_OS_MAC
-    /** Handle tray icon clicked */
-    void trayIconActivated(QSystemTrayIcon::ActivationReason reason);
-#endif
 
     /** Show window if hidden, unminimize when minimized, rise when obscured or show if hidden and fToggleHidden is true */
     void showNormalIfMinimized() { showNormalIfMinimized(false); }
