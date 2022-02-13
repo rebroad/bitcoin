e4a44be573006b6a45780f138f1c9e247dc75526
R E Broadley <github.com@esuza.com>
2022-02-13 19:18:14 +0000

    ok

--- src/qt/trafficgraphwidget.h
+++ src/qt/trafficgraphwidget.h
@@ -53,7 +53,7 @@ private:
     float fMax;
     float new_fMax;
     float fMins;
-    int new_fMins;
+    float new_fMins;
     int nValue;
     QQueue<float> vSamplesIn[VALUES_SIZE];
     QQueue<float> vSamplesOut[VALUES_SIZE];
