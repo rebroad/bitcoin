46f6d126e9989eff608d16f4b9e6534a03363da0
R E Broadley <github.com@esuza.com>
2022-02-16 08:08:13 +0000

    fix

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -1174,7 +1174,7 @@ void RPCConsole::updateTrafficStats(quint64 totalBytesIn, quint64 totalBytesOut)
     if (!slider_in_use) {
         std::chrono::minutes mins = ui->trafficGraph->getGraphRange();
         static std::chrono::minutes last_mins = mins;
-        if (mins == last_mins + 1) {
+        if (mins.count() == last_mins.count() + 1) {
             LogPrintf("%s: Bump it up! mins: %d->%d\n", __func__, last_mins.count(), mins.count());
             setTrafficGraphRange(0); // bump it up
             last_mins = mins;
