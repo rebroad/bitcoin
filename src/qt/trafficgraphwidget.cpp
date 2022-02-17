bb9f4af185f883490b9a20a1aa41474288c91557
R E Broadley <github.com@esuza.com>
2022-02-17 12:28:44 +0000

    increment debug

--- src/qt/trafficgraphwidget.cpp
+++ src/qt/trafficgraphwidget.cpp
@@ -277,6 +277,7 @@ bool update_num(float new_val, float &current, float &increment, int length)
     if (abs(increment) >= 0.8 * current / length)
         current += increment;
     else {
+        LogPrintf("%s: final jump. inc=%d > 0.8 * %d / %d\n", __func__, abs(increment), current, length);
         current = new_val;
         increment = 0;
     }
