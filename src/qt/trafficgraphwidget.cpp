44acabe9d9148292ea1617f35a63d09d4fe0c9de
R E Broadley <github.com@esuza.com>
2022-03-02 08:24:38 +0100

    better

--- src/qt/trafficgraphwidget.cpp
+++ src/qt/trafficgraphwidget.cpp
@@ -272,8 +272,8 @@ bool update_num(float new_val, float &current, float &increment, int length)
                 ((increment < 0) && (current + increment * 2 < new_val))) {
             increment = increment / 2; // Keep the momentum going even if new_val is elsewhere.
         } else {
-            if (((increment > 0) && (current + increment * 4 < new_val)) ||
-                    ((increment < 0) && (current + increment * 4 > new_val))) {
+            if (((increment > 0) && (current + increment * 8 < new_val)) ||
+                    ((increment < 0) && (current + increment * 8 > new_val))) {
                 increment = increment * 2;
             }
         }
