a2e91dc7dfdaa9e85c5bd4a33e3443698d3f71a4
R E Broadley <github.com@esuza.com>
2022-03-02 16:09:13 +0100

    runttoggle

--- src/net.cpp
+++ src/net.cpp
@@ -1719,11 +1719,17 @@ void CConnman::SocketHandlerConnected(const std::vector<CNode*>& nodes,
         } else if (nPeersIBD) IsIBD = true;
     } // if (now != lastnow)
 
-    bool fDownloadBlocks = gArgs.GetBoolArg("-downloadblocks", true);
-    int nTechnique = fDownloadBlocks ? (now / 10800) % 2 : 0; // 0 = Pct, 1 = TXpm
+    int nTechnique = 0;
     bool fLatestNodePctDegrading = false;
     bool fLatestNodeTXpmDegrading = false;
     if (!IsIBD && lastnow != now) {
+        bool fDownloadBlocks = gArgs.GetBoolArg("-downloadblocks", true);
+        int nRuntToggle = gArgs.GetIntArg("-runttoggle", 180) * 60;
+        if (nRuntToggle < 2)
+            nTechnique = nRuntToggle;
+        else
+            nTechnique = fDownloadBlocks ? (now / nRuntToggle) % 2 : 0; // 0 = Pct, 1 = TXpm
+
         if (worstNodeTXpmBTXpm > nLowestBTXpm) {
             worstNodeTXpm = worstNodeBTXpm;
             nLowestTXpm = nLowestBTXpm;
