cf37d7e0b42ea2ca144c337bc23279868eb47375
R E Broadley <github.com@esuza.com>
2022-05-13 23:59:13 +0100

    ok

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -1148,7 +1148,7 @@ CFeeRate CTxMemPool::GetMinFee(size_t sizelimit) const {
             rollingMinimumFeeRate = rollingMinimumFeeRate / fraction + newrolling * ((fraction - 1) / fraction);
             if (rollingMinimumFeeRate < 0)
                 rollingMinimumFeeRate = 0;
-        } else if (bumpedMinimumFeeRate > rollingMinimumFeeRate)
+        } else if (bumpedMinimumFeeRate > rollingMinimumFeeRate && newrolling >= bumpedMinimumFeeRate)
             bumpedMinimumFeeRate = rollingMinimumFeeRate;
         lastRollingFeeUpdate = time;
     }
