557dc56bec33b6a1f751f3ba55558df92c6e42c7
R E Broadley <github.com@esuza.com>
2023-01-27 18:57:05 +0000

    fix divide by zero

--- src/net.cpp
+++ src/net.cpp
@@ -1730,8 +1730,8 @@ void CConnman::SocketHandlerConnected(const std::vector<CNode*>& nodes,
                 } else if (nBTXpm && nBTXpm < nSecondLowestBTXpm)
                     nSecondLowestBTXpm = nBTXpm;
             } else if (pnode->IsInboundConn()) {
-                int nRecvBps = 8 * nRecvBytes / (now + 1 - m_connected);
-                int nSendBps = 8 * nSendBytes / (now + 1 - m_connected);
+                int nRecvBps = 8 * nRecvBytes / std::min(now - m_connected, 1);
+                int nSendBps = 8 * nSendBytes / std::min(now - m_connected, 1);
                 if ((now - m_connected >= 120) && (nMempoolPct < 10) && ((nRecvBps > 120) || (nSendBps > 1200))) {
                     if (!pnode->HasPermission(NetPermissionFlags::NoBan)) {
                         pnode->fDisconnect = 1;
