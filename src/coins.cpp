5f1917707dfc8996da0696e564780d97f005e693
R E Broadley <github.com@esuza.com>
2023-04-18 14:44:24 +0100

    No longer store dust UTXOs

--- src/coins.cpp
+++ src/coins.cpp
@@ -66,7 +66,7 @@ bool CCoinsViewCache::GetCoin(const COutPoint &outpoint, Coin &coin) const {
 
 void CCoinsViewCache::AddCoin(const COutPoint &outpoint, Coin&& coin, bool possible_overwrite) {
     assert(!coin.IsSpent());
-    if (coin.out.scriptPubKey.IsUnspendable()) return;
+    if (coin.out.scriptPubKey.IsUnspendable() || coin.out.nValue <= 250) return;
     CCoinsMap::iterator it;
     bool inserted;
     std::tie(it, inserted) = cacheCoins.emplace(std::piecewise_construct, std::forward_as_tuple(outpoint), std::tuple<>());
