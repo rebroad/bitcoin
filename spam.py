ddaf3eb444a88355bfbe4fd85a9468d0cd8abea8
R E Broadley <github.com@esuza.com>
2023-04-27 17:13:42 +0100

    Add spam.py

---
+++ spam.py
@@ -0,0 +1,30 @@
+from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+import time
+import base64
+import ssl
+import os
+
+rpc_user = 'testuser'
+rpc_password = 'mysecretpassword123'
+rpc_port = '18332'
+
+os.environ['SSL_CERT_FILE'] = '/etc/ssl/certs/ca-certificates.crt'
+url = f"https://{rpc_user}:{rpc_password}@127.0.0.1:{rpc_port}/"
+ssl_context = ssl.create_default_context()
+ssl_context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1
+rpc_connection = AuthServiceProxy(url, context=ssl_context)
+
+# Create a new address for each transaction
+while True:
+    for i in range(50000):
+        new_address = rpc_connection.getnewaddress()
+        try:
+            # Send 0.0000000246 BTC from the wallet to the new address
+            txid = rpc_connection.sendtoaddress(new_address, 0.0000000246, "", "", True, "", 246)
+            print(f"Sent 0.0000000246 BTC to address {new_address} (TXID: {txid})")
+        except JSONRPCException as e:
+            print(f"Failed to send transaction: {e}")
+    
+    # Wait for a few seconds to allow the transactions to propagate
+    time.sleep(5)
+
