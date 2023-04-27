935d8606cdaaef48bee12df3eb1f7cf5f9cf243c
R E Broadley <github.com@esuza.com>
2023-04-27 17:26:05 +0100

    spam.py

--- spam.py
+++ spam.py
@@ -1,37 +1,17 @@
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 import time
-import ssl
-import os
-import http.client
 
 class CustomAuthServiceProxy(AuthServiceProxy):
-    def __init__(self, service_url, context=None, timeout=None):
-        if context is not None:
-            if not isinstance(context, ssl.SSLContext):
-                raise TypeError("context must be an instance of ssl.SSLContext")
-            self.context = context
+    def __init__(self, service_url, timeout=None):
         self.timeout = timeout
         AuthServiceProxy.__init__(self, service_url, timeout=timeout)
-
-    def _get_connection(self):
-        if self.context is not None:
-            return http.client.HTTPSConnection(
-                self.service_url.hostname, self.service_url.port, context=self.context, timeout=self.timeout
-            )
-        else:
-            return http.client.HTTPConnection(
-                self.service_url.hostname, self.service_url.port, timeout=self.timeout
-            )
 
 rpc_user = 'testuser'
 rpc_password = 'mysecretpassword123'
 rpc_port = '18332'
 
-os.environ['SSL_CERT_FILE'] = '/etc/ssl/certs/ca-certificates.crt'
-url = f"https://{rpc_user}:{rpc_password}@127.0.0.1:{rpc_port}/"
-ssl_context = ssl.create_default_context()
-ssl_context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1
-rpc_connection = CustomAuthServiceProxy(url, context=ssl_context)
+url = f"http://{rpc_user}:{rpc_password}@127.0.0.1:{rpc_port}/"
+rpc_connection = CustomAuthServiceProxy(url)
 
 # Create a new address for each transaction
 while True:
