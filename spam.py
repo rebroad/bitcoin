ca4c0f1b6431c93efaf8cbeec74b4b56cda57d26
R E Broadley <github.com@esuza.com>
2023-04-27 17:34:07 +0100

    spam

--- spam.py
+++ spam.py
@@ -19,7 +19,7 @@ while True:
         new_address = rpc_connection.getnewaddress()
         try:
             # Send 0.0000000246 BTC from the wallet to the new address
-            txid = rpc_connection.sendtoaddress(new_address, 0.0000000246, "", "", True, "", True)
+            txid = rpc_connection.sendtoaddress(new_address, 0.0000000246, "", "", True, True, 246)
             print(f"Sent 0.0000000246 BTC to address {new_address} (TXID: {txid})")
         except JSONRPCException as e:
             print(f"Failed to send transaction: {e}")
