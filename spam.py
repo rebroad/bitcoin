2e22f49ff5097886a05f290ff808aa6ab4dbeec7
R E Broadley <github.com@esuza.com>
2023-04-27 17:48:25 +0100

    spam

--- spam.py
+++ spam.py
@@ -18,12 +18,26 @@ while True:
     for i in range(50000):
         new_address = rpc_connection.getnewaddress()
         try:
-            # Send 0.0000000246 BTC from the wallet to the new address
-            txid = rpc_connection.sendtoaddress(new_address, 0.0000000246, "", "", True, True, 246)
-            print(f"Sent 0.0000000246 BTC to address {new_address} (TXID: {txid})")
+            # Create a raw transaction with no inputs and one output
+            raw_tx = rpc_connection.createrawtransaction([], {new_address: 0.000003})
+
+            # Fund the raw transaction, setting the fee to 1 satoshi
+            funded_tx = rpc_connection.fundrawtransaction(raw_tx, {"feeRate": 0, "subtractFeeFromOutputs": [0]})
+            tx_fee = int(funded_tx["fee"] * 100000000)  # Convert to satoshis
+            if tx_fee > 1:
+                tx_fee = 1
+
+            # Set the fee to 1 satoshi
+            funded_tx = rpc_connection.fundrawtransaction(raw_tx, {"feeRate": 0, "replaceable": True, "subtractFeeFromOutputs": [0], "fee_amount": tx_fee / 100000000})
+
+            # Sign the funded transaction
+            signed_tx = rpc_connection.signrawtransactionwithwallet(funded_tx["hex"])
+
+            # Send the signed transaction
+            txid = rpc_connection.sendrawtransaction(signed_tx["hex"])
+            print(f"Sent 0.000003 BTC to address {new_address} (TXID: {txid})")
         except JSONRPCException as e:
             print(f"Failed to send transaction: {e}")
 
     # Wait for a few seconds to allow the transactions to propagate
     time.sleep(5)
-
