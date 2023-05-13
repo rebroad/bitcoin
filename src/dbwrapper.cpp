f20734c9dc25b1fb03115e3aada32f27eb25f62c
R E Broadley <rebroad@users.noreply.github.com>
2023-05-13 10:04:09 +0100

    undo

--- src/dbwrapper.cpp
+++ src/dbwrapper.cpp
@@ -130,7 +130,6 @@ CDBWrapper::CDBWrapper(const fs::path& path, size_t nCacheSize, bool fMemory, bo
     } else {
         if (fWipe) {
             LogPrintf("Wiping LevelDB in %s\n", fs::PathToString(path));
-            assert(!fWipe);
             leveldb::Status result = leveldb::DestroyDB(fs::PathToString(path), options);
             dbwrapper_private::HandleError(result);
         }
