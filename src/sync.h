8556fcc22a8fb1f154ea9f4641e91bded294bc70
R E Broadley <rebroad@users.noreply.github.com>
2025-03-07 22:44:03 +0000

    Fix try_lock error

--- src/sync.h
+++ src/sync.h
@@ -144,11 +144,11 @@ private:
     bool TryEnter(const char* pszName, const char* pszFile, int nLine)
     {
         EnterCritical(pszName, pszFile, nLine, Base::mutex(), true);
-        Base::try_lock();
-        if (!Base::owns_lock()) {
+        bool locked = Base::try_lock();
+        if (!locked) {
             LeaveCritical();
         }
-        return Base::owns_lock();
+        return locked;
     }
 
 public:
