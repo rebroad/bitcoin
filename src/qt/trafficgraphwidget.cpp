1a6d2faa81b7bb38eba5dda78df141e51e1eb9d2
R E Broadley <rebroad@users.noreply.github.com>
2025-05-04 10:32:24 +0100

    tgw fixup

--- src/qt/trafficgraphwidget.cpp
+++ src/qt/trafficgraphwidget.cpp
@@ -65,13 +65,14 @@ int TrafficGraphWidget::paintPath(QPainterPath& path, const QQueue<float>& sampl
         { // x is already calculated for the first sample
             double ratio = static_cast<double>(i) * m_values[m_value] / m_range / (DESIRED_SAMPLES - 1);
             x = XMARGIN + static_cast<int>(w - w * ratio + 0.5);
-            if (i < 1 || i == sample_count-1)
-                printf("%s: i=%d, x=%d, w=%d, ratio=%f, m_value=%d, m_range=%f\n",
-                    __func__, i, x, w, ratio, m_values[m_value], m_range);
+            int old_x = x;
             if (i == sample_count-1 && (x <= XMARGIN || (samples.size() >= DESIRED_SAMPLES && ratio < 1.0))) {
                 path.lineTo(x, y_value(samples.at(i)));
                 x--; // Overscan by one pixel to the left
             }
+            if (i == sample_count-1)
+                printf("%s: i=%d, x=%d->%d, w=%d, ratio=%f, m_value=%d, m_range=%f\n",
+                    __func__, i, old_x, x, w, ratio, m_values[m_value], m_range);
         }
         path.lineTo(x, y_value(samples.at(i)));
     }
@@ -254,7 +255,7 @@ void TrafficGraphWidget::paintEvent(QPaintEvent *)
 
     // Draw black lines to mask the bright overscanned edges
     painter.setPen(Qt::black);
-    painter.drawLine(x, YMARGIN, XMARGIN - 1, YMARGIN + h);
+    painter.drawLine(x, YMARGIN, x, YMARGIN + h);
     painter.drawLine(XMARGIN + w + 1, YMARGIN, XMARGIN + w + 1, YMARGIN + h);
 
     // Draw the bottom axis line and labels after the graph
