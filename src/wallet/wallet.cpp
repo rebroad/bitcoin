976744dd88a68970f32899a80ad5771d35588b01
R E Broadley <rebroad@users.noreply.github.com>
2025-05-27 16:42:14 +0100

    fixup

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3323,7 +3323,10 @@ void CWallet::SetupDescriptorScriptPubKeyMans()
 
         if (!signer_res.isObject()) throw std::runtime_error(std::string(__func__) + ": Unexpected result");
         for (bool internal : {false, true}) {
-            UniValue descriptor_vals = find_value(signer_res, internal ? "internal" : "receive");
+            #pragma GCC diagnostic push
+            #pragma GCC diagnostic ignored "-Wdangling-reference"
+            const UniValue& descriptor_vals = find_value(signer_res, internal ? "internal" : "receive");
+            #pragma GCC diagnostic pop
             if (!descriptor_vals.isArray()) throw std::runtime_error(std::string(__func__) + ": Unexpected result");
             for (const UniValue& desc_val : descriptor_vals.get_array().getValues()) {
                 std::string desc_str = desc_val.getValStr();
