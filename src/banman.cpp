06948acc9b90044a6069fe9683e77aabb0adec5e
R E Broadley <rebroad@users.noreply.github.com>
2026-02-07 14:52:08 -0500

    Don't increase ban count for entries already banned

--- src/banman.cpp
+++ src/banman.cpp
@@ -163,11 +163,9 @@ void BanMan::Ban(const CSubNet& sub_net, int64_t ban_time_offset, bool since_uni
                 ban_entry.m_ban_count = existing_entry.m_ban_count + 1;
                 LogPrint(BCLog::BANMAN, "Address %s on probation banned again, ban duration: %d seconds\n",
                          sub_net.ToString(), normalized_ban_time_offset);
-            } else if (existing_entry.m_ban_count > 0) {
-                // Not on probation but has been banned before, increment count
+            } else if (current_time >= existing_entry.nBanUntil)
+                // Not currently banned
                 ban_entry.m_ban_count = existing_entry.m_ban_count + 1;
-            } else {
-                ban_entry.m_ban_count = 1;
             }
         } else {
             ban_entry.m_ban_count = 1;
